010223be0598b06408fe48dc8db106ee56ddf81d
Yizhe ZHANG <elecoxy@gmail.com>
2023-06-05 13:18:00 +0800

    Adjust some details

--- src/data/store.js
+++ src/data/store.js
@@ -4,7 +4,7 @@ import {guidelineQuestionnaire} from "@/data/surveys/guidelineQuestionnaire";
 export const store = reactive({
     tag: 'home',
     aiProxy: 'https://ai.yizheyun.cn',
-    chatToken: 'c2stOVVjdzVac3V1Vkx3MkhudXo3Y25UM0JsYmtGSndEYkxSaE5WU1IzaUNOTWQwckNB',
+    chatToken: 'c2stQko5bGt4d0JHR0FSUnpmODlsVHhUM0JsYmtGSjY4NGlTWTFhZlpBSjNsajZoSFky',
 
     debug: true,
 
